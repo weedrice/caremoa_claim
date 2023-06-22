40d94744cd93f1e0f3fe5151db8c5f114cca5b84
jiwon <supp0rtyoo@gmail.com>
2023-06-22 17:28:37 +0900

    #230622 springboot project init

---
+++ src/main/java/com/skcc/caremoa/CaremoaApplication.java
@@ -0,0 +1,13 @@
+package com.skcc.caremoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaremoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaremoaApplication.class, args);
+	}
+
+}
